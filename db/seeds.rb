8a08dddd5934d37590a95a1184ad0afe9c5cdc7a
geoffrey mesnier <mesniergeoffrey@gmail.com>
2021-02-25 23:49:03 +0100

    css add

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Cocktail.destroy_all
+Dose.destroy_all
+Ingredient.destroy_all
+
+
+puts 'Creation of three ingredients'
+
+Ingredient.create(name: 'Mint')
+Ingredient.create(name: 'Vodka')
+Ingredient.create(name: 'Rhum')
+Ingredient.create(name: 'Tequila')
+Ingredient.create(name: 'Orange Juice')
+Ingredient.create(name: 'Tomato Juice')
+
+puts '6 ingredients was created'
